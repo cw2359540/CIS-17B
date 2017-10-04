e584f7e4ab8c9774e20712cecfab1294b6711320
Cheryllynn <cwalsh5@student.rcc.edu>
2017-10-04 13:24:19 -0700

    Signed-off-by: Cheryllynn <cwalsh5@student.rcc.edu>

---
+++ lab/BattleshipV1/View.js
@@ -0,0 +1,23 @@
+/* 
+    Created on : Sep 18, 2017, 6:28:09 PM
+    Author     : Head First Javascript Example Project
+ */
+
+
+var view = {
+    displayMessage: function(msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+
+    displayHit: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+
+    displayMiss: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+
+}; 
